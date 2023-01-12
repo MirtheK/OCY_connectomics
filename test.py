a23a1f64ce32d76a8f039364ef986e0bef61a8ba
MirtheK <80322675+MirtheK@users.noreply.github.com>
2023-01-12 14:11:08 +0100

    changes

--- test.py
+++ test.py
@@ -5,7 +5,19 @@ Created on Mon Jan  9 10:45:15 2023
 @author: Mirthe
 """
 
-import matlab.engine
+# import matlab.engine
+from mayavi import mlab
+import numpy as np
 
-eng = matlab.engine.start_matlab()
+data = np.random.random((100,100,100))
+
+mlab.pipeline.volume(mlab.pipeline.scalar_field(data))
+mlab.show()
+
+# eng = matlab.engine.start_matlab()
+
+# # eng = matlab.engine.start_matlab("Users\Mirthe\Docments\GiHub\OCY_connectomics\OCY_main")
+
+# eng.Skel2Graph3D(nargout=0)
+# eng.quit()
 
